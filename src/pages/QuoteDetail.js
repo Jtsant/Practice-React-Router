c6112b12c746ac93cee5b5b8fc3af7dda0469d8c
Jtsant <jtsant92@hotmail.com>
2022-08-17 16:22:06 +0300

    Redirecting Params

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,15 @@
-const QuoteDetail = () => {
-    return <h1>Quote Detail Page</h1>
-};
+import { useParams } from "react-router-dom";
+import { Fragment } from "react";
 
+const QuoteDetail = () => {
+  const params = useParams();
+
+  return (
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  );
+};
 
 export default QuoteDetail;
