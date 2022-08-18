234de2c14b5e147c892eca68d57c352ab1377913
Jtsant <jtsant92@hotmail.com>
2022-08-18 12:55:13 +0300

    More Content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Max', text: 'Learnign React is fun!'},
+    {id: 'q2', author: 'Maximilian', text: 'Learnign React is great!'},
+];
+
 const AllQuotes = () => {
-    return <h1>All Quotes Page</h1>
+    return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 
